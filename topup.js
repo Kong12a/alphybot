11b6975557b9a93e302201ad025878cd81d12e72
Kong12a <ideekai9@gmail.com>
2024-09-16 19:19:08 +0700

    update

--- topup.js
+++ topup.js
@@ -19,6 +19,7 @@ app.post('/topup', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
